4d16cdaa01af4a9b2cac58e28d58aa03a93d6dcd
nhoj <nhoj@MacBook-Pro.local>
2023-09-28 08:12:39 +0800

    procfile configure

--- Professional_Website/asgi.py
+++ Professional_Website/asgi.py
@@ -19,8 +19,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Professional_Website.settings')
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
-    # 'http':get_asgi_application(),
-    'https':get_asgi_application(),
+    'http':get_asgi_application(),
+    # 'https':get_asgi_application(),
 
      'websocket':AuthMiddlewareStack(
          URLRouter(
