036fbd63774e08852973af5efb19acc0bfec1b22
nhoj <nhoj@MacBook-Pro.local>
2023-11-02 23:52:58 +0800

    again

--- CHAT_app/consumers.py
+++ CHAT_app/consumers.py
@@ -1,28 +1,46 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
-
+from .models import Chat_Room,Chat_Messages
+import time
 class ChatConsumer(WebsocketConsumer):
+
+    print('NEWNEWNEW')
     def connect(self):
-        self.accept()
+        self.accept() 
         current_room = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = current_room
         
         # When A user Joins the Room
         self.receive([['A Person Joined','Anonymous'],'Name of Persone'])
+
+        self.chatRoom = Chat_Room.objects.get_or_create(chat_room_name=self.room_group_name)[0]
+
         #  ... Get Regustered User str(self.scope["user"]) 
         self.chat_message([f'\n You Join the room {current_room}','Unregister User'])
         # 
+        print('\n\n Messages: ',self.chatRoom.messages_items)
+        print('Message: ',self.chatRoom.chat_room_name)
+        for i in self.chatRoom.messages_items:
+            self.chat_message([i.sender_chat_message,i.sender_name])
+            time.sleep(0.3)
+
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+
+        # Creating or Getting ChatRoom
 
 
         # self.send(text_data=json.dumps({
         #     'type':'connection_established',
         #     'message':'You are now Connected!'
         # }))
+    def get_chat_room(self):
+        pass
+
+
 
     def receive(self, text_data):
 
@@ -30,6 +48,19 @@ class ChatConsumer(WebsocketConsumer):
             text_data_json = json.loads(text_data)
             message =  text_data_json['message']
             username =  text_data_json['username']
+            print('\n Chat messaged')
+            new_chat_message = Chat_Messages()
+            new_chat_message.sender_ID = username
+            new_chat_message.sender_name = username
+            new_chat_message.sender_chat_message = message
+            new_chat_message.save()
+            new_chat_message.chat_room_model = self.chatRoom
+            self.chatRoom.chatroom_messages.add(new_chat_message)
+            self.chatRoom.save()
+            
+            # .objects.create(sender_ID=username,sender_name=username,sender_chat_message=message,chat_room_model=self.chatRoom)
+            # new_chat_message.save()
+            print('\n CHat Saved')
         except:
             message = text_data[0]
             username = text_data[1]
--- CHAT_app/models.py
+++ CHAT_app/models.py
@@ -1,4 +1,22 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
-    
+from datetime import datetime
+
+
+class Chat_Room(models.Model):
+    admin_ID = models.CharField(max_length=10)
+    admin_name = models.CharField(max_length=64)
+    chat_room_name = models.CharField(max_length=128)
+    chatroom_messages = models.ManyToManyField('CHAT_app.Chat_Messages',blank=True, related_name='chat_model_room')
+    @property
+    def messages_items(self):
+        return self.chatroom_messages.all()
+
+class Chat_Messages(models.Model):
+    sender_ID = models.CharField(max_length=32)
+    sender_name = models.CharField(max_length=32)
+    sender_image = models.URLField(blank=True)
+    sender_chat_message = models.TextField(blank=True)
+    chat_room_model = models.ForeignKey('CHAT_app.Chat_Room',on_delete=models.CASCADE,null=True)
+    message_timestamp = models.DateTimeField(auto_now_add=True)
