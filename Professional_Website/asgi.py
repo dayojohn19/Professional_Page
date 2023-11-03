7974f00fd7a01097ff4b9e3858fe17a344042e7c
nhoj <nhoj@MacBook-Pro.local>
2023-11-03 09:45:17 +0800

    asgi added import django and django setup

--- Professional_Website/asgi.py
+++ Professional_Website/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
@@ -16,7 +17,7 @@ from channels.auth import AuthMiddlewareStack
 import CHAT_app.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Professional_Website.settings')
-
+django.setup()
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
