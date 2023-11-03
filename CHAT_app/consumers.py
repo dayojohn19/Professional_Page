fc09f1d74ca52ee1d1834b2939d0e25ca0ad5bf7
nhoj <nhoj@MacBook-Pro.local>
2023-11-03 13:36:16 +0800

    again

--- CHAT_app/consumers.py
+++ CHAT_app/consumers.py
@@ -73,7 +73,7 @@ class ChatConsumer(WebsocketConsumer):
             new_chat_message.chat_room_model = self.chatRoom
             self.chatRoom.chatroom_messages.add(new_chat_message)
             self.chatRoom.save()
-            
+            message_timestamp = new_chat_message.message_timestamp
             # .objects.create(sender_ID=username,sender_name=username,sender_chat_message=message,chat_room_model=self.chatRoom)
             # new_chat_message.save()
             print('\n CHat Saved')
@@ -87,7 +87,7 @@ class ChatConsumer(WebsocketConsumer):
                 'type':'chat_message',
                 'message':message,
                 'username':username,
-                'message_timestamp':new_chat_message.message_timestamp
+                'message_timestamp':message_timestamp
             }
         )
         # self.send(text_data=json.dumps({
