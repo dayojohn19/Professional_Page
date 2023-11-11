4aeeb5138f95fbee8bb73408c0e946bada21be18
nhoj <nhoj@MacBook-Pro.local>
2023-11-11 22:46:58 +0800

    added admin

--- CHAT_app/admin.py
+++ CHAT_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Chat_Messages_Model, Chat_Room_Model
+
+admin.site.register(Chat_Messages_Model)
+admin.site.register(Chat_Room_Model)
